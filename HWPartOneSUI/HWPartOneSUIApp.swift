4f2468da8d2d5b0fba69e8ecb929faa35cc8d724
Mark Wilgelm <markujke@mail.ru>
2022-08-26 10:35:11 +0200

    Initial Commit

---
+++ HWPartOneSUI/HWPartOneSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWPartOneSUIApp.swift
+//  HWPartOneSUI
+//
+//  Created by Mark Wilhelm on 26.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct HWPartOneSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
